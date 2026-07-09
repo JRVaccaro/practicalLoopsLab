34fffab40c951238d902d3381a5e22551fb49d33
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-09 11:03:48 -0400

    answered first question to second part

--- loopdaloops.js
+++ loopdaloops.js
@@ -17,15 +17,57 @@ If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 If a number is not divisible by either 3 or 5, log the number.*/ 
 
 
-for( let i = 0; i < 101; i++){
-    console.log(i);
-if(i % 3 === 0 && i % 5 === 0){
-    console.log("Fizz Buzz")
-} else if( i % 5 === 0){
-    console.log("Buzz")
-} else if (i % 3 === 0){
-    console.log("Fizz")
-}else{
-    console.log(i)
+//for( let i = 0; i < 101; i++){
+   // console.log(i);
+//if(i % 3 === 0 && i % 5 === 0){
+  //  console.log("Fizz Buzz")
+//} else if( i % 5 === 0){
+  //  console.log("Buzz")
+//} else if (i % 3 === 0){
+    //console.log("Fizz")
+//}else{
+    //console.log(i)
+//}
+//}
+
+
+/* Context: A prime number is any whole number greater than 1 that cannot 
+be exactly divided by any whole number other than itself and 1. For example, 
+the number 5 is prime because it cannot be divided by 4, 3, or 2; 
+it can only be divided by itself (5) and 1. Similarly, 
+the numbers 7 and 11 are prime. As numbers become larger, 
+determining whether or not they are prime is increasingly difficult, 
+but loops make this process relatively easy!
+
+Write a script that accomplishes the following:
+
+Declare an arbitrary number, n.
+
+Create a loop that searches for the next prime number, starting at n and 
+incrementing from there.
+
+
+As soon as you find the prime number, log that number and exit the loop.
+
+Continuing with the example above, if n is equal to 4, your loop should log 5. 
+Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
+Test your loop with higher numbers and reference an online prime number table to
+ determine the accuracy of your code.
+
+Be careful! If you set n to a number too large, your loop could take a long time to process.*/
+
+let n = 3;
+
+let isPrime = true; 
+
+if(n <= 1){
+    isPrime = false;
+
 }
-}
+    for(let i = 2; i < n; i++){
+    if(n % i === 0){
+        isPrime = false;
+        break;
+        
+} console.log(isPrime)
+    }
